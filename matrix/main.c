0b7d4a2a879ec6d0d63cd13c03efafa43406b0dc
MateusGVSoares <mateus.gon30@gmail.com>
2022-06-28 11:08:22 -0300

    Resolvendo as novas listas

--- matrix/main.c
+++ matrix/main.c
@@ -8,8 +8,8 @@
 // Definição da estrutura de matriz
 typedef struct
 {
-    int rows; // Número de linhas
-    int cols; // Número de colunas
+    int rows;     // Número de linhas
+    int cols;     // Número de colunas
     int **matriz; // Ponteiro para a matriz
 
 } Matrix;
@@ -18,7 +18,6 @@ int alloc_matrix(Matrix *target, int rows, int cols)
 {
     target->cols = cols;
     target->rows = rows;
-
 
     // Aloca (rows) ponteiros para as linhas e verifica se a memória foi alocada com sucesso
     target->matriz = malloc(rows * sizeof(int *));
@@ -35,19 +34,18 @@ int alloc_matrix(Matrix *target, int rows, int cols)
     return 0;
 }
 
-
 // Free na memória alocada para a matriz
-int free_matrix(Matrix * target)
+int free_matrix(Matrix *target)
 {
-    for(int m = 0; m < target->rows; m++)
+    for (int m = 0; m < target->rows; m++)
     {
-            // Deve liberar a memória de cada ponteiro da linha individualmente 
-            free(target->matriz[m]); 
+        // Deve liberar a memória de cada ponteiro da linha individualmente
+        free(target->matriz[m]);
     }
     // Libera a memória alocada para os ponteiros de ponteiros
     free(target->matriz);
     target->matriz = NULL;
-    return 0; 
+    return 0;
 }
 
 // Atribui um valor individual para uma entrada da matriz
@@ -74,31 +72,29 @@ int set_matrix_row(Matrix *target_m, int *vector, int row)
         return 1;
 }
 
-int sum_matrix(Matrix * matriz_a, Matrix * matriz_b,Matrix * result)
+int sum_matrix(Matrix *matriz_a, Matrix *matriz_b, Matrix *result)
 {
-	// Verifica a alocação de memória
-	if(matriz_a == NULL || matriz_b == NULL)
-		return -1;
+    // Verifica a alocação de memória
+    if (matriz_a == NULL || matriz_b == NULL)
+        return -1;
 
-	// Se a soma não conseguir ser realizada, retorna 1
-	if(matriz_a->cols != matriz_b->cols || matriz_a->rows != matriz_b->rows)
-		return 1;
+    // Se a soma não conseguir ser realizada, retorna 1
+    if (matriz_a->cols != matriz_b->cols || matriz_a->rows != matriz_b->rows)
+        return 1;
 
-
-	for(int i_rows = 0; i_rows < matriz_a->rows; i_rows++)
-	{
-		for(int i_cols = 0; i_cols < matriz_a->cols; i_cols++)
-		{
-			result->matriz[i_rows][i_cols] = matriz_a->matriz[i_rows][i_cols] + matriz_b->matriz[i_rows][i_cols];
-		}
-	}
-	return 0;
+    for (int i_rows = 0; i_rows < matriz_a->rows; i_rows++)
+    {
+        for (int i_cols = 0; i_cols < matriz_a->cols; i_cols++)
+        {
+            result->matriz[i_rows][i_cols] = matriz_a->matriz[i_rows][i_cols] + matriz_b->matriz[i_rows][i_cols];
+        }
+    }
+    return 0;
 }
 
-
-int mult_matrix(Matrix * matriz_a, Matrix * matriz_b,Matrix * result)
+int mult_matrix(Matrix *matriz_a, Matrix *matriz_b, Matrix *result)
 {
-    if(matriz_a->cols != matriz_b->rows)
+    if (matriz_a->cols != matriz_b->rows)
     {
         return 1;
     }
@@ -111,22 +107,22 @@ int mult_matrix(Matrix * matriz_a, Matrix * matriz_b,Matrix * result)
 
     int mult_result = 0, it_cols = 0, it_cols_b;
     // Itera as linhas da matriz A
-    for(int i_rows = 0; i_rows < matriz_a->rows; i_rows++)
+    for (int i_rows = 0; i_rows < matriz_a->rows; i_rows++)
     {
-        // Itera as colunas de B 
-            for(int it_cols_b= 0; it_cols_b < matriz_b->cols; it_cols_b++)
+        // Itera as colunas de B
+        for (int it_cols_b = 0; it_cols_b < matriz_b->cols; it_cols_b++)
         {
             // Itera as colunas da linha i_rows da matriz A
-            for(it_cols = 0; it_cols < matriz_a->cols; it_cols++)
+            for (it_cols = 0; it_cols < matriz_a->cols; it_cols++)
             {
                 // Guarda numa variável a multiplicação dos elementos da linha i_rows A e da coluna it_cols_b B
-                mult_result += (matriz_a->matriz[i_rows][it_cols])*(matriz_b->matriz[it_cols][it_cols_b]);
+                mult_result += (matriz_a->matriz[i_rows][it_cols]) * (matriz_b->matriz[it_cols][it_cols_b]);
             }
 #ifdef DEBUG
             printf("Iterando na posição [%d][%d] (Resultado = %d) \n",
-                i_rows,
-                it_cols,
-                mult_result);
+                   i_rows,
+                   it_cols,
+                   mult_result);
 #endif
             result->matriz[i_rows][it_cols_b] = mult_result;
             mult_result = 0;
@@ -136,58 +132,72 @@ int mult_matrix(Matrix * matriz_a, Matrix * matriz_b,Matrix * result)
 }
 
 // Retorna o vetor com as médias de cada coluna
-float * get_rows_avg(Matrix * target)
+float *get_rows_avg(Matrix *target)
 {
     // Cria um vetor para armazenar as médias
-    float * avg_vet = malloc(sizeof(int) * target->rows);
-    int avg =0;
+    float *avg_vet = malloc(sizeof(int) * target->rows);
+    int avg = 0;
 
-    for(int i = 0; i < target->rows; i++)
+    for (int i = 0; i < target->rows; i++)
     {
         avg = 0;
-        for(int j = 0; j < target->cols ; j++)
+        for (int j = 0; j < target->cols; j++)
             avg += target->matriz[i][j];
-        avg_vet[i] = (float)avg/target->cols;
+        avg_vet[i] = (float)avg / target->cols;
     }
     return avg_vet;
 }
 
-int find_max_val(float * avg_vet, int size, float * ret_value)
+int find_max_val(float *avg_vet, int size, float *ret_value)
 {
     float max_value = 0.0;
     int max_index = 0;
 
-    for(int i =0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
-        if(avg_vet[i] > max_value)
+        if (avg_vet[i] > max_value)
         {
             max_value = avg_vet[i];
-            max_index = i;    
+            max_index = i;
         }
     }
     *(ret_value) = max_value;
     return max_index;
 }
 
+float upper_matrix_op(Matrix *target, int op)
+{
+    float ret_val = 0;
+    for (int i = 0; i < target->rows / 2; i++)
+    {
+        for (int j = i + 1; j < (target->cols - (i + 1)); j++)
+        {
+            ret_val += target->matriz[i][j];
+        }
+    }
+    if (op)
+        ret_val /= 30.0;
+    return ret_val;
+}
+
 /* UTILIDADES */
 
 // Gera uma matriz aleatória de acordo com range e offset
-void rand_matrix(Matrix * target,int range, int offset)
+void rand_matrix(Matrix *target, int range)
 {
-    // Preenche a matriz de tamanho MxN com elementos randômicos 
-    for(int i = 0; i < target->rows; i++)
+    // Preenche a matriz de tamanho MxN com elementos randômicos
+    for (int i = 0; i < target->rows; i++)
     {
-        for(int j = 0; j < target->cols; j++)
-            target->matriz[i][j] = offset + (rand()%range);
+        for (int j = 0; j < target->cols; j++)
+            target->matriz[i][j] = (rand() % range);
     }
 }
-
 
 // Função para printar a matriz
 void matrix_to_string(Matrix target)
 {
     int *it_row = NULL;
-    printf("\n");
+    printf("{\n");
 
     for (int row_c = 0; row_c < target.rows; row_c++)
     {
@@ -197,19 +207,19 @@ void matrix_to_string(Matrix target)
         for (int cols_c = 0; cols_c < target.cols; cols_c++)
         {
             // Printa o conteúdo da posição de coluna
-            printf("%d ", it_row[cols_c]);
+            printf("[%d]", it_row[cols_c]);
         }
         printf("\n");
     }
-    printf("\n");
+    printf("}\n");
 }
 
-// Printa a string float 
-void print_vector(float * vector, int size)
+// Printa a string float
+void print_vector(float *vector, int size)
 {
-    for(int i = 0 ; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
-        printf("%0.4f, ",vector[i]);
+        printf("%0.4f, ", vector[i]);
     }
     printf("\n");
 }
@@ -218,21 +228,14 @@ int main()
 {
     Matrix *target_a = malloc(1 * sizeof(Matrix));
 
-    if (alloc_matrix(target_a, 7, 12))
+    if (alloc_matrix(target_a, 12, 12))
     {
         printf("DEU RUIM ! \n");
         return 0;
     }
-    
-    srand(time(0));
 
-    rand_matrix(target_a,200,100);
-    matrix_to_string(*(target_a));
-    
-    float * ret_pt = get_rows_avg(target_a);
-    float ret_val = 0.0;
-    print_vector(ret_pt, target_a->rows);
-    int find_pos = find_max_val(ret_pt,target_a->rows,&ret_val);
-    printf("\n Mes = %d / Valor = %0.4f \n",find_pos + 1,ret_val);
-    free_matrix(target_a);
+    // srand(time(0));
+    rand_matrix(target_a, 2);
+    matrix_to_string(*target_a);
+    printf("Resultado = %f \n", upper_matrix_op(target_a, 0));
 }
--- pointers/main.c
+++ pointers/main.c
@@ -1,100 +1,313 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-char * strsrch(char * pointer, char ch)
+// Obtem o menor número
+int get_lnumber(int *array, int size, int *ret_pointer)
 {
-  char * ret_pointer = NULL;
-  for(int i = 0; (pointer[i] != '\0'); i++)
-  { 
-    if(pointer[i] == ch)
-      ret_pointer= &pointer[i]; 
-  }
+    int i = 0,
+        ret_pos = 0,
+        ret_value = array[size - 1];
+
+    // Varre o vetor procurando o menor valor
+    for (; i < size - 1; i++)
+    {
+        if (array[i] < ret_value)
+        {
+            ret_value = array[i];
+            ret_pos = i;
+        }
+    }
+
+    // Retorna os valores
+    *ret_pointer = ret_value;
+    return ret_pos;
+}
+
+// Obtêm o maior número
+int get_gnumber(int *array, int size)
+{
+    int ret_number = array[size - 1];
+    for (int i = 0; i < size - 1; i++)
+    {
+        if (array[i] > ret_number)
+            ret_number = array[i];
+    }
+    return ret_number;
+}
+
+int str_size(char *arr_s)
+{
+    int i = 0;
+    for (; arr_s[i] != '\0'; i++)
+        ;
+    return i;
+}
+
+char *strsrch(char *pointer, char ch)
+{
+    char *ret_pointer = NULL;
+    for (int i = 0; (pointer[i] != '\0'); i++)
+    {
+        if (pointer[i] == ch)
+            ret_pointer = &pointer[i];
+    }
     return ret_pointer;
 }
 
-char * f_strsrch(char * pointer, char ch)
+char *f_strsrch(char *pointer, char ch)
 {
-  char * ret_pointer = NULL;
-  for(int i = 0; (pointer[i] != '\0'); i++)
-  { 
-    if(pointer[i] == ch)
+    char *ret_pointer = NULL;
+    for (int i = 0; (pointer[i] != '\0'); i++)
     {
-      ret_pointer= &pointer[i];
-      break;
+        if (pointer[i] == ch)
+        {
+            ret_pointer = &pointer[i];
+            break;
+        }
     }
-  }
     return ret_pointer;
 }
 
-int ** create_ptr(int * array,int size)
+// Obtem se os n ultimos elementos de str_1 sao iguais a str_2
+int verf_condition(char *str_1, int size_1, char *str_2, int size_2)
 {
-  int ** res_array = malloc(sizeof(int *) * size);
-  int * aux_pointer = NULL;
-  int  cont = 1;
-
-  // Copia os enderecos do array inicial para o vetor auxiliar
-  for(int i = 0; i < size; i++)
-    res_array[i] = &array[i];
-
-  // Sort no vetor de ponteiros
-  while(cont)
-  {
-    cont = 0;
-    for(int pos = 0; pos < size - 1; pos++)
+    if (size_1 < size_2)
+        return 0;
+    for (int i = size_1 - size_2, j = 0; i < size_1; i++, j++)
     {
-      if(*(res_array[pos]) > *(res_array[pos + 1]))
-      {
-        aux_pointer = res_array[pos];
-        res_array[pos] = res_array[pos + 1];
-        res_array[pos+1] = aux_pointer; 
-        cont++;
-      }
+        if (str_1[i] != str_2[j])
+            return 0;
     }
-  }
-
-  return res_array;
+    return 1;
 }
 
-void fill_vector(int * array,int size)
+void to_lower(char *str)
 {
-  for(int i = 0; i < size; i++)
-    array[i] = rand()%30;
+    // Varre todo o vetor até o terminador de string
+    for (int i = 0; str[i] != '\0'; i++)
+        if (str[i] >= 0x41 && str[i] <= 0x5A)
+            // Incrementa o valor para casar com o minusculo em ASCII
+            str[i] += 0x20;
 }
 
-void print_vector(char * name,int * array, int size)
+// Questão 4, pega os caracteres com mais ocorrência da string
+void get_max_c(char *array)
 {
-  printf("%s[%d] = {",name,size);
+    int *vet_letras = malloc(sizeof(int) * 26);
+    memset(vet_letras, 0, 26);
+    int max_val = 0;
+    // Transforma a string em apenas letras minúsculas
+    to_lower(array);
+    // printf("%s \n", array);
 
-  for(int i = 0; i < size; i++)
-  {
-    printf("%d,",array[i]);
-  }
-  printf("};\n");
+    // Percorre todo o vetor contando as letras
+    for (int i = 0; array[i] != '\0'; i++)
+    {
+        if (array[i] >= 0x61 && array[i] <= 0x7A)
+            vet_letras[(int)(array[i]) - 0x61]++;
+    }
+    // print_vector("Vet_Letras", vet_letras, 26);
+
+    // Pega o maior numero de ocorrências e printa as letras com essa ocorrência
+    max_val = get_gnumber(vet_letras, 26);
+    //  printf("Maior ocorrência : %d \n", max_val);
+    for (int i = 0; i < 26; i++)
+    {
+        if (vet_letras[i] == max_val)
+            printf("%c", (0x61 + i));
+    }
+    free(vet_letras);
 }
 
-void print_ptr(char * name,int ** array,int size)
-{ 
-  printf("%s[%d] = {",name,size);
+char *get_code(char *target_str)
+{
+    // Ola Casa
+    int vet_pos = 0;
+    for (int i = 0; target_str[i] != '\0'; vet_pos++, i++)
+    {
+        // Get next letter
+        while (target_str[i] == ' ')
+            i++;
 
-  for(int i = 0; i < size; i++)
-  {
-    printf("%d,",*(array[i]));
-  }
-  printf("};\n");
+        printf("Fct  : %c | Pos : %d | Vet_Pos : %d \n", target_str[i], i, vet_pos);
+        target_str[vet_pos] = target_str[i];
+
+        // Get next space
+        while (target_str[i] != ' ' && target_str[i] != '\0')
+            i++;
+    }
+    printf("%d \n", vet_pos);
+    target_str[vet_pos] = '\0';
+    return target_str;
 }
 
-#define _SIZE 10
+// Função que obtêm o número de aliterações em uma sequência de palavras
+int get_allit(char *target_str)
+{
+    // Numero para retornar a quantidade de aliterações
+    int ret_allit = 0;
+
+    // Cria um vetor para incrementar as respectivas letras
+    int *vet_letras = malloc(sizeof(int) * 26);
+    memset(vet_letras, 0, 26);
+
+    // Transforma a string em caracteres minúsculos
+    to_lower(target_str);
+
+    for (int i = 0; target_str[i] != '\0'; i++)
+    {
+        if (target_str[i + 1] != ' ')
+            vet_letras[target_str[i] - 0x61]++;
+        while (target_str[i] != ' ' && target_str[i] != '\0')
+            i++;
+    }
+
+    for (int i = 0; i < 26; i++)
+        if (vet_letras[i] > 1)
+        {
+            ret_allit++;
+        }
+
+    // Libera a memória e retorna o valor
+    free(vet_letras);
+    return ret_allit;
+}
+
+void fill_vector(int *array, int size)
+{
+    for (int i = 0; i < size; i++)
+        array[i] = rand() % 30;
+}
+
+void print_vector(char *name, int *array, int size)
+{
+    printf("%s[%d] = {", name, size);
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d,", array[i]);
+    }
+    printf("};\n");
+}
+
+void print_ptr(char *name, int **array, int size)
+{
+    printf("%s[%d] = {", name, size);
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d,", *(array[i]));
+    }
+    printf("};\n");
+}
+
+int **create_ptr(int *array, int size)
+{
+    int **res_array = malloc(sizeof(int *) * size);
+    int *aux_pointer = NULL;
+    int cont = 1;
+
+    // Copia os enderecos do array inicial para o vetor auxiliar
+    for (int i = 0; i < size; i++)
+        res_array[i] = &array[i];
+
+    // Sort no vetor de ponteiros
+    while (cont)
+    {
+        cont = 0;
+        for (int pos = 0; pos < size - 1; pos++)
+        {
+            if (*(res_array[pos]) > *(res_array[pos + 1]))
+            {
+                aux_pointer = res_array[pos];
+                res_array[pos] = res_array[pos + 1];
+                res_array[pos + 1] = aux_pointer;
+                cont++;
+            }
+        }
+    }
+
+    return res_array;
+}
+
+int get_number(int *array, int a_size, int n_pos)
+{
+    int ret_number = 0, // Número para retornar
+        aux_lower = 0,  // Variável que contêm o menor valor
+        atual_cont = 0; // Varíavel de contagem dos ciclos
+
+    // Obtêm o maior valor e salva em ret_number
+    ret_number = get_gnumber(array, a_size);
+    get_lnumber(array, a_size, &aux_lower);
+    printf("Maior número = [%d] | Menor número = [%d] \n", ret_number, aux_lower);
+
+    // Repete a varredura do vetor enquanto nao obter o n maior elemento ou percorer a_size elementos
+    for (int atual_pos = 1, atual_lower = aux_lower; (atual_pos < n_pos) && (atual_cont < a_size); atual_cont++)
+    {
+        atual_lower = aux_lower;
+        // Varre o vetor
+        for (int i = 0; i < a_size; i++)
+        {
+            // Procura pelo n maior elemento
+            if (array[i] > atual_lower && array[i] < ret_number)
+            {
+                atual_lower = array[i];
+            }
+        }
+
+        if (atual_lower >= aux_lower) //  Verifica se o número é maior que o menor
+        {
+            // ret_number vira agora o n-1 maior
+            ret_number = atual_lower;
+            atual_pos++;
+            // printf("%d_maior = [%d] | N+1 maior = [%d] \n", atual_pos, aux_lower, ret_number);
+        }
+    }
+    printf("%d \n", atual_cont);
+    if (atual_cont >= a_size)
+        return -1;
+    return ret_number;
+}
+
+int check_repeat(int *array, int size, int value, int ig_pos)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (i != ig_pos && array[i] == value)
+            return 1;
+    }
+    return 0;
+}
+
+void gen_norepeat(int *array, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        while (check_repeat(array, size, array[i], i))
+            array[i] = rand() % 50;
+    }
+}
 
 int main()
 {
-  srand(time(0));
-  int * input_vec = malloc(sizeof(int) * _SIZE);
-  int ** ret_vet = NULL;
-  
-  fill_vector(input_vec,_SIZE);
-  print_vector("INPUT",input_vec,_SIZE);  
-  ret_vet = create_ptr(input_vec, _SIZE);
-  print_ptr("RESULT",ret_vet,_SIZE);
-  return 0;
+    int test_cases = 0;
+    printf("Digite o numero de casos : ");
+    test_cases = getchar() - 0x30;
+    printf("%d \n", test_cases);
+    while (getchar() != '\n')
+        ;
+
+    char *str_main = malloc(sizeof(char) * 100);
+    for (int cases = 0; cases < test_cases; cases++)
+    {
+        printf("Digite a string : ");
+        fgets(str_main, 100, stdin);
+        str_main = get_code(str_main);
+        printf("%s\n", str_main);
+    }
+
+    free(str_main);
+    return 0;
 }
